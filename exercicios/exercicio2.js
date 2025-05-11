7a87df6b0607e7643bddc5d61a8aa3868fb15a78
jotaze09 <jl275010@gmail.com>
2025-05-10 21:24:12 -0300

    exercicio2.js

--- exercicios/exercicio2.js
+++ exercicios/exercicio2.js
@@ -1,3 +1,15 @@
 let valor;
+let descontoAplicado;
 valor = prompt ("qual o valor da sua compra ?");
+
+if (descontoAplicado>=100) {
+    descontoAplicado = 10
+} else {
+    descontoAplicado = 5
+}
+let total = (valor / 100 * descontoAplicado);
+let valorFinal = (valor - total);
+alert (valorFinal);
+
+
 
