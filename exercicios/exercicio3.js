5fcb7c332e82f84be9599fe8c29e9399ac85f6c8
jotaze09 <jl275010@gmail.com>
2025-05-29 14:56:32 -0300

    Delete exercicios directory

--- exercicios/exercicio3.js
+++
@@ -1,8 +0,0 @@
-let idade;
-idade = prompt("Informe sua idade:");
-
-if (idade>=18) {
-    alert ('acesso liberado!')
-} else { 
-    alert ('acesso negado!')
-}
--- exercicios/exercicio5.js
+++
@@ -1,8 +0,0 @@
-let comfirmar;
-confirmar = confirm ("excluir conta ?")
-
-if (confirmar) {
-  alert("Conta excluída com sucesso");
-} else { 
-  alert("Ação cancelada pelo usuário");
-}
